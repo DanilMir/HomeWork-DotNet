f60e300cae1bcdb70457cbd9a27d6fbc980c6a4f
DanilMir <54032222+DanilMir@users.noreply.github.com>
2021-09-20 11:04:57 +0300

    Revert "2k 200"

--- src/Homework1/Homework1/Program.cs
+++ src/Homework1/Homework1/Program.cs
@@ -7,9 +7,6 @@ namespace Homework1
     {
         public static int Main(string[] args)
         {
-            if (CheckArgsCount(args) != 0)
-                return 3;
-            
             var parseResult = Parser.TryParseArguments(args, out var val1, out var operation, out var val2);
             if (parseResult != 0)
             {
@@ -18,17 +15,6 @@ namespace Homework1
 
             var result = Calculator.Calculate(operation, val1, val2);
             Console.WriteLine($"{args[0]} {args[1]} {args[2]} = {result}");
-
-            return 0;
-        }
-
-        private static int CheckArgsCount(string[] args)
-        {
-            if (args.Length != 3)
-            {
-                Console.WriteLine("The number of arguments must be 3");
-                return 3;
-            }
 
             return 0;
         }
