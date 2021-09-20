5d85e0d0bdf87e9c3f14fd7b267f508704222867
DanilMir <mirgayazov02@gmail.com>
2021-09-20 17:36:09 +0300

    fix: 100% coverage, coverage result to xml file

--- src/Homework1/Homework1.Tests/ProgramTests.cs
+++ src/Homework1/Homework1.Tests/ProgramTests.cs
@@ -23,6 +23,7 @@ namespace Homework1.Tests
         [InlineData(new string[] {"", "", ""}, 1)]
         [InlineData(new string[] {""}, 3)]
         [InlineData(new string[] {"", "", "", ""}, 3)]
+        [InlineData(new string[] {"0", "/", "0"}, 4)]
         public void Main_InvalidArguments(string[] args, int excepted)
         {
             var actual = Program.Main(args);
