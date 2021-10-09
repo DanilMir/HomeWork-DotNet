0e0917ab546a9797fa14fa2752221eecb48f3308
DanilMir <mirgayazov02@gmail.com>
2021-10-09 19:53:47 +0300

    add: calculator module

---
+++ src/Homework4/Calculator.fs
@@ -0,0 +1,10 @@
+﻿namespace Homework4
+
+module Calculator =
+    let Calculate val1 operation val2 =
+        match operation with
+        | "+" -> val1 + val2
+        | "-" -> val1 - val2
+        | "*" -> val1 * val2
+        | "/" -> val1 / val2
+        | _ -> 0
