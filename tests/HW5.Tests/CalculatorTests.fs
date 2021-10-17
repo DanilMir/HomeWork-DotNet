b0bdcbb201788bddeb4023a2a4cf68dc597246e6
DanilMir <mirgayazov02@gmail.com>
2021-10-17 13:01:17 +0300

    edit: calctests

--- tests/HW5.Tests/CalculatorTests.fs
+++ tests/HW5.Tests/CalculatorTests.fs
@@ -11,6 +11,10 @@ open HW5.Result
 [<InlineData(1, "-", 2, -1)>]
 [<InlineData(1, "*", 2, 2)>]
 [<InlineData(1, "/", 1, 1)>]
+[<InlineData(1.0, "+", 2.2, 3.2)>]
+[<InlineData(1.23, "-", 2.23, -1)>]
+[<InlineData(2.5, "*", 2, 5)>]
+[<InlineData(1.0, "/", 1.0, 1.0)>]
 let ``Calculate valid`` val1 operand val2 expected =
     let actual = Calculate val1 operand val2
     Assert.Equal(Success expected, actual)
