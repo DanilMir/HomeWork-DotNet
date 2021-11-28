d49e0f4b65854788097958649cf432ba97585dca
DanilMir <mirgayazov02@gmail.com>
2021-11-28 14:15:39 +0300

    add: unit test

---
+++ src/HW9/Controllers/CalculatorController.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq.Expressions;
+using HW9.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HW9.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        [HttpGet, Route("calc")]
+        public IActionResult Calc(string expr)
+        {
+            var temp = Parser.Parse(expr);
+            var visit = new Visitor().Visit(temp);
+            var t = (int) (new Visitor().Visit(temp) as ConstantExpression)?.Value!;
+            return Ok(t);
+        }
+
+    }
+}
--- src/HW9/Startup.cs
+++ src/HW9/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HW9.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -51,7 +52,7 @@ namespace HW9
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}");
             });
         }
     }
