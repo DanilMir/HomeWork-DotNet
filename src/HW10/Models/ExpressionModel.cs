afa578efb3baf6f9840b4bcab9943d4a17ef932e
DanilMir <mirgayazov02@gmail.com>
2021-12-05 18:43:01 +0300

    add: expression model

---
+++ src/HW10/Models/ExpressionModel.cs
@@ -0,0 +1,10 @@
+﻿namespace HW10.Models
+{
+    public class ExpressionModel
+    {
+        public int Id { get; set; }
+        public int Value1 { get; set; }
+        public int Value2 { get; set; }
+        public int Operation { get; set; }
+    }
+}
