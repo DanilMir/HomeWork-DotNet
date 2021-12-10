863bf13d34879cb5f1061bea78958f4a1cb8086b
DanilMir <mirgayazov02@gmail.com>
2021-12-10 15:03:16 +0300

    exceptions

---
+++ src/HW11/Services/IExceptionHandler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+namespace HW11.Services
+{
+    public interface IExceptionHandler
+    {
+        public void HandleException<T>(T exception) where T : Exception;
+    }
+}
