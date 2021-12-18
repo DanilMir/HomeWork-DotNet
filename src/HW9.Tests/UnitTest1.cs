a6456982fc4b19253a6ec23d51e3ac9dc487d4be
DanilMir <mirgayazov02@gmail.com>
2021-12-18 18:36:08 +0300

    try fixing

--- src/HW9.Tests/UnitTest1.cs
+++ src/HW9.Tests/UnitTest1.cs
@@ -20,9 +20,9 @@ namespace HW9.Tests
 
         private const string ResponseBody = "https://localhost:5001/calc?expr=";
 
-        [Theory, InlineData("1+2+3+4+5", 2, 15), InlineData("2/2", 2, 1), InlineData("(2+1)/2", 2, 1)]
-        [InlineData("(2+3)/12*7+8*9", 5, 72), InlineData("1-2+3", 2, 2)]
-        [InlineData("1/(2+3)", 2, 0)]
+        [Theory, InlineData("1+2+3+4+5", 3, 15), InlineData("2/2", 2, 1), InlineData("(2+1)/2", 3, 1)]
+        [InlineData("(2+3)/12*7+8*9", 5, 72), InlineData("1-2+3", 3, 2)]
+        [InlineData("1/(2+3)", 3, 0)]
         public async Task TimeTest(string expression, int timeInSeconds, decimal answer)
         {
             var client = _factory.CreateClient();
