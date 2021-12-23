9c0b8e15297a28e81b99a9a9c0701025570e80ac
DanilMir <mirgayazov02@gmail.com>
2021-12-23 11:50:11 +0300

    HW13 init

---
+++ src/HW13/TestMethods.cs
@@ -0,0 +1,39 @@
+﻿namespace HW13;
+
+class TestMethods
+{
+    public string Method(string input)
+    {
+        input += input;
+        return input;
+    }
+
+    public virtual string VirtualMethod(string input)
+    {
+        input += input;
+        return input;
+    }
+
+    public static string StaticMethod(string input)
+    {
+        input += input;
+        return input;
+    }
+
+    public string GenericMethod<T>(T input)
+    {
+        return input.ToString() + input;
+    }
+
+    public string DynamicMethod(dynamic input)
+    {
+        input += input.ToString();
+        return input;
+    }
+
+    public string ReflectionMethod(string input)
+    {
+        input += input;
+        return input;
+    }
+}
